6717f2f94ad00ada35900b65cffa872ec070a56f
ashap127 <porwalasha58@gmail.com>
2025-08-10 19:34:49 +0530

    app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: "https://trashtotrashur-frontend.onrender.com",
+  origin: "http://localhost:5173",
   credentials: true
 }));
 
