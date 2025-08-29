cfb155e9f0fb8ef4bc81e17445279a0bfa4af86c
ashap127 <porwalasha58@gmail.com>
2025-08-29 22:42:51 +0530

    error fix

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ const app = express();
 // ===== Middleware =====
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    // origin: "http://localhost:5173",
+      origin: "https://trashtotrashur-frontend.onrender.com",
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     credentials: true,
   })
